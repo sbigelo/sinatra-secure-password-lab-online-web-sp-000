cf4ded992916f89b52bf900392afdb6b670702ba
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-19 19:22:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,6 @@ class ApplicationController < Sinatra::Base
       User.create(:username => params[:username], :password => params[:password])
       redirect "/login"
     end
-
-
-
   end
 
   get '/account' do
@@ -39,9 +36,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
+    @user = User.find_by(:username => params[:username])
 
-	  if user && user.authenticate(params[:password])
+	  if @user && @user.authenticate(params[:password])
 	    session[:user_id] = user.id
 	    redirect "/account"
 	  else
